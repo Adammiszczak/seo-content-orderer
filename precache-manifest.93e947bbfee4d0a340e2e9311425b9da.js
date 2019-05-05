a0b9123b6c59365d1a4140260c8f556fde09a84a
Adam Miszczak <adammiszczak1@gmail.com>
2019-05-05 15:29:58 +0200

    Updates

---
+++ precache-manifest.93e947bbfee4d0a340e2e9311425b9da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43c9cc1a666bbfb0d729f23ac49d276",
+    "url": "/seo-content-orderer/index.html"
+  },
+  {
+    "revision": "bd38dfafa2336ea9ce3f",
+    "url": "/seo-content-orderer/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/seo-content-orderer/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bd38dfafa2336ea9ce3f",
+    "url": "/seo-content-orderer/static/js/main.cb3fb8cf.chunk.js"
+  },
+  {
+    "revision": "754a992a8907df0549f5",
+    "url": "/seo-content-orderer/static/js/runtime~main.f61bd575.js"
+  }
+]);
